ecf06b75ce1acfacf7d5acaab3d835b330478c00
Alex Spernau <alexspernau.online@gmail.com>
2026-05-22 23:16:52 +0200

    Add enemy, camera, spawn and respawn scripts
    Add new gameplay scripts and scene/prefab bindings: EnemyBehavior (enemy AI/impulse), CameraBehavior (simple follow with offset), RespawnTrigger (destroy enemies / log player fall) and a stub SpawnManager, plus their .meta files. Update PlayerController to add speed/turn handling, movement inputs, rotation while moving forward, and a dash that modifies speed on Space press/release. Update prefabs and the main scene (Assets/Alex Dummies/Prefabs/Gegner.prefab and BuildingBalance1.unity) to attach EnemyBehavior to the Gegner prefab, add SpawnManager and RespawnTrigger components, add CameraBehavior to the Camera (with player reference and offset), and adjust a few prefab instance properties/positions. Add an "Enemy" tag to ProjectSettings/TagManager.asset.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Definiert Geschwindigkeit
+    public float speed = 10;
+    // Definiert Rotations Geschwindigkeit
+    public float turnSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Curser-Tasten rechts u links abfragen (Wert zwischen -1 u 1)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // Curser-Tasten oben u unten abfragen (Wert zwischen -1 u 1)
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // GameObject entlang der X-Achse verschieben
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        // GameObject entlang der Z-Achse verschieben
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // GameObject entlang der Y-Achse rotieren
+        if (verticalInput > 0)
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        }
+        // Dash funktion bauen
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed *= 2;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            speed = 20;
+        }
     }
+
+
 }
+
+
+
