f838b2afcb86969b7ae41c1dd52842aaef7933dc
nightwatcher23 <maren@rautmann.eu>
2026-05-23 23:20:05 +0200

    Update Player Script + Camera Scipt
    Player-Movement + Dash works now, Rotation has to be revisited
    Camera now looks in the right direction, adaptable rotation needs more work

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,13 +4,19 @@ public class CameraBehavior : MonoBehaviour
 {
     // Referenz zum Spieler
     public GameObject player;
+
     // Abstand zum Spieler
     public Vector3 offset;
 
     // Update is called once per frame
     void LateUpdate()
     {
+        //Kamera folgt Spieler
         transform.position = player.transform.position + offset;
+
+        //Rotation passt sich der Spieler-Rotation an 
+        //PROBLEM: Funktioniert nur so mäßig, muss überarbeitet werden
+        transform.rotation = player.transform.rotation;
 
     }
    
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     //Definiert Geschwindigkeit
     public float speed = 10;
+    private float finalSpeed;
+
     // Definiert Rotations Geschwindigkeit
     public float turnSpeed;
 
@@ -15,18 +17,29 @@ public class PlayerController : MonoBehaviour
         // Curser-Tasten oben u unten abfragen (Wert zwischen -1 u 1)
         float verticalInput = Input.GetAxis("Vertical");
 
+        //Dash Funktion (Über Space? Sicher?)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            finalSpeed = speed * 2;
+        }
+        else
+        {
+            finalSpeed = speed;
+        }
+
         // GameObject entlang der X-Achse verschieben
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * finalSpeed * horizontalInput);
 
         // GameObject entlang der Z-Achse verschieben
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * finalSpeed * verticalInput);
 
         // GameObject entlang der Y-Achse rotieren
+        //PROBLEM: Objekt rotiert, Kamera allerdings nicht
         if (verticalInput > 0)
         {
             transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
         }
-        // Dash funktion bauen
+        /* Dash funktion bauen
         if (Input.GetKeyDown(KeyCode.Space))
         {
             speed *= 2;
@@ -34,7 +47,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             speed = 20;
-        }
+        }*/
     }
 
 
