3a381f3236d9c221c90126eabbca4c57ecbbc103
nightwatcher23 <maren@rautmann.eu>
2026-05-24 17:56:00 +0200

    Fixed Camera
    Free rotating camera implemented, control with mouse

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -5,41 +5,53 @@ public class CameraBehavior : MonoBehaviour
     // Referenz zum Spieler
     public GameObject player;
 
-    /*// Abstand zum Spieler
-    public Vector3 offset;*/
+    // Abstand zum Spieler
+    public Vector3 offset;
 
-    private const float YMin = -50.0f;
-    private const float YMax = 50.0f;
+    //Geschwindigkeit der Drehbewegung
+    public float turnSpeed = 4.0f;
 
-    public float distance = 10;
-    private float currentX = 0;
-    private float currentY = 0;
-    public float sensitivity = 4;
+    //Abstand zum Player
+    private float playerDistance;
 
+    //Drebewegung einschränken
+    public float minTurnAngle = -90;
+    public float maxTurnAngle = 0;
+    private float rotationY;
 
-    // Update is called once per frame
+    void Start()
+    {
+        //Abstand Kamera zum Spieler speichern
+        playerDistance = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    void Update()
+    {
+        //Maus-Angaben speichern
+        float mouseX = Input.GetAxis("Mouse X") * turnSpeed;
+
+        rotationY += mouseX;
+
+        /*//Vertikale Rotation einschränken
+        mouseY = Mathf.Clamp(mouseY, minTurnAngle, maxTurnAngle);*/
+
+        //Kamera rotieren
+        transform.eulerAngles = new Vector3(15, rotationY, 0);
+
+        //Kamera-Position anpassen
+        transform.position = player.transform.position - (transform.forward * playerDistance/2);
+    }
+
+    /*// Update is called once per frame
     void LateUpdate()
     {
-        currentX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        currentY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-
-        currentY = Mathf.Clamp(currentY, YMin, YMax);
-
-        Vector3 direction = new Vector3(0, 0, -distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = player.transform.position + rotation * direction;
-
-        transform.player(player.position);
-
-
-
-        /*//Kamera folgt Spieler
+        //Kamera folgt Spieler
         transform.position = player.transform.position + offset;
 
         //Rotation passt sich der Spieler-Rotation an 
         //PROBLEM: Funktioniert nur so mäßig, muss überarbeitet werden
-        transform.rotation = player.transform.rotation;*/
+        transform.rotation = player.transform.rotation;
 
-    }
+    }*/
    
 }
