2c303821781133aca5bdeab500d1d2648abc256f
Alex <alexspernau.online@gmail.com>
2026-05-24 16:29:21 +0200

    weight scale1

--- Assets/Scripts/WeightScaleController.cs
+++ Assets/Scripts/WeightScaleController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ScaleController : MonoBehaviour
 {
+    public enum Weights
+    { Value1, Value2, Value3 }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
