51803a22f21139d3a71acae300acff9b35d0e5e2
Alex Spernau <alexspernau.online@gmail.com>
2026-05-25 10:36:23 +0200

    RespawnTrigger auskommentiert

--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -5,16 +5,17 @@ public class RespawnTrigger : MonoBehaviour
     //Methode wird aufgerufen, wenn ein Collider in den Trigger eintritt
     void OnTriggerEnter(Collider other)
     {
+        // Überprüft, ob das Objekt, das den Trigger betreten hat, ein Gegner oder der Spieler ist
         if (other.gameObject.CompareTag("Enemy"))
         {
-            Destroy(other.gameObject); // Destroy the enemy game object
+            Destroy(other.gameObject); // Der Gegner wird zerstört, wenn er in den Trigger fällt
             Debug.Log("Enemy has died!");
 
         }
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player has fallen into the ball Pit!");
-            // Here you can add code to handle the player's death, such as restarting the level or showing a game over screen.
+            //Hier kannst du Code hinzufügen, um den Respawn des Spielers zu behandeln, z.B. das Level neu zu starten oder einen Game-Over-Bildschirm anzuzeigen.
         }
     }
 }
