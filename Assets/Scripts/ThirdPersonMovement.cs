4ecaa782f32781882b5dafdf00d489fb911a9932
nightwatcher23 <maren@rautmann.eu>
2026-05-31 14:00:27 +0200

    Player can move up ramps

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,6 +8,11 @@ public class ThirdPersonMovement : MonoBehaviour
     public float speed = 6f;
     public float jumpForce = 5f;
     private bool isGrounded = true;
+    public float rotationSpeed = 5f;
+
+    private Vector3 movementInput;
+    private float targetAngle;
+    private Vector3 targetDirection;
 
     public float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
@@ -15,21 +20,23 @@ public class ThirdPersonMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Variablen für Spielersteuerung WASD
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        //Richtungsvektor für Bewegung, normalized damit auch diagonale Bewegung gleich schnell ist + Variablen für Spielersteuerung WASD
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+        //targetDirection = new Vector3(transform.position.x, 0, transform.position.z);
 
-        //Richtungsvektor für Bewegung, normalized damit auch diagonale Bewegung gleich schnell ist
-        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        //playerRb.transform.rotation = Quaternion.Slerp(playerRb.transform.rotation, Quaternion.LookRotation(-targetDirection), rotationSpeed * Time.deltaTime);
 
         //Gleichmäßige Drehbewegung des Spielers
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.rotation.eulerAngles.y;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0, angle, 0);
+        if (movementInput.magnitude > 0)
+        {
+            targetAngle = Mathf.Atan2(movementInput.x, movementInput.z) * Mathf.Rad2Deg + camera.rotation.eulerAngles.y; //gezielter World-space Winkel des Spielers basierend auf Kamera (in Richtung Kamera) und User-Input
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //für gleichmäßige Drehung
+            transform.rotation = Quaternion.Euler(0, angle, 0); //tatsächliche Rotation
+        }
 
         //Bewegung des Spielers in Kamera-Richtung
-        Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-        if(horizontalInput != 0f || verticalInput != 0f)
+        Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; //Kamera-Richtung
+        if (movementInput.magnitude > 0) //Bewegung wird nur bei Player-Input ausgeführt
         {
             //Sprint-Funktion auf linker Shift/Hochstell-Taste
             float finalSpeed;
@@ -41,18 +48,20 @@ public class ThirdPersonMovement : MonoBehaviour
             {
                 finalSpeed = speed;
             }
-            
+
+            //tatsächliche Bewegung
             playerRb.transform.Translate(moveDirection.normalized * finalSpeed * Time.deltaTime);
         }
 
         //Hüpf-Funktion auf Leertaste
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
     }
 
+    //Verhindert, dass Spieler Doppelsprung macht
     public bool PlayerIsGrounded()
     {
         isGrounded = true;
