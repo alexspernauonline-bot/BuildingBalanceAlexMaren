1d7ef880aceca659b5186bb57c20e23275256a70
nightwatcher23 <maren@rautmann.eu>
2026-07-14 11:59:58 +0200

    FPCC in progress

---
+++ Assets/Scripts/Playground_Scripts/FirstPersonPlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FirstPersonPlayerController : MonoBehaviour
+{
+    public LayerMask castingMask;
+
+    private CharacterController controller;
+    private Transform playerTx;
+
+    public Transform cameraTx;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
