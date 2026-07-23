ae4c175b5ddb916452b69655e8b7e574a18f14e2
nightwatcher23 <maren@rautmann.eu>
2026-07-23 13:55:35 +0200

    Running + Crouching added to player movement

--- Assets/Scripts/Playground_Scripts/PlayerMovement.cs
+++ Assets/Scripts/Playground_Scripts/PlayerMovement.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Verbindung zum Character controller
     public CharacterController controller;
 
-    public float speed = 12f;
+    //Bewegungsvariablen
+    public float normalSpeed = 12f;
+    private float actualSpeed;
     public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public float jumpHeight = 2f;
+    public float normalHeight = 2;
 
+    //Variablen für GroundCheck
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-
-    Vector3 velocity;
     bool isGrounded;
-    
+
+    //Geschwindigkeit
+    Vector3 velocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,27 +30,54 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Ground Check
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        //Zurücksetzen der Schwerkraft, wenn Player den Boden berührt (nach unten gerichtete Velocity)
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        //Tastatureingaben speichern (WASD/Pfeiltasten)
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        //Bewegungsvektor
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        //Sprint Bewegung (Shift)
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            actualSpeed = normalSpeed * 1.5f;
+            controller.height = normalHeight;
+        }
+        //Crouch Bewegung (Ctrl)
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            actualSpeed = normalSpeed * 0.5f;
+            controller.height = normalHeight * 0.5f;
+        }
+        //Normale Bewegung
+        else
+        {
+            actualSpeed = normalSpeed;
+            controller.height = normalHeight;
+        }
 
+        //Bewegung des Players
+        controller.Move(move * actualSpeed * Time.deltaTime);
+
+        //Jump Funktion (Leertaste)
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        //Erhöhung der Schwerkraft
         velocity.y += 1.5f * gravity * Time.deltaTime;
 
+        //Schwerkraft beeinflusst Spieler
         controller.Move(velocity * Time.deltaTime);
     }
 }
