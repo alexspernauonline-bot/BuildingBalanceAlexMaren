1720a72f0103003060a3c4f5704ed2ce1cfa1335
nightwatcher23 <maren@rautmann.eu>
2026-07-24 16:36:23 +0200

    Moving plattforms added to parkour

--- Assets/Scripts/Playground_Scripts/MovingPlattforms.cs
+++ Assets/Scripts/Playground_Scripts/MovingPlattforms.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class MovingPlattforms : MonoBehaviour
 {
+    //Festgelegte Punkte
+    private int zA = -20;
+    private int zB = 10;
+
+    private Vector3 pointA;
+    private Vector3 pointB;
+
+    //Geschwindigkeit
+    public float speed = 3;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Bewegung nur entlang der y-Achse
+        pointA = new Vector3(transform.position.x, transform.position.y, zA);
+        pointB = new Vector3(transform.position.x, transform.position.y, zB);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Loop zwischen PointA und PointB für kontinuierliche Bewegung der Plattform
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time / speed, 1));
     }
 }
