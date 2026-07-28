cecb5ac9bda1cc96140e76f6d3d4ac0ef8abfcba
nightwatcher23 <maren@rautmann.eu>
2026-07-28 10:20:18 +0200

    all buttons click

--- Assets/Scripts/UI-Scrips/ClickButtonScript.cs
+++ Assets/Scripts/UI-Scrips/ClickButtonScript.cs
@@ -1,22 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class ClickButtonScript : MonoBehaviour
 {
+    //Verbindung zum Audio
     private AudioSource audioSource;
     public AudioClip clickSound;
-    public Button returnBtn;
+
+    //Menü-Button Refs
+    /*public Button returnBtnHTP;
+    public Button returnBtnSettings;
+    public Button returnBtnEndTitle;
+    public Button closeBtn;
+    public Button startBtn;
+    public Button settingsBtn;
+    public Button guideBtn;*/
+
+    public Button[] btns = new Button[7];
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        returnBtn.onClick.AddListener(PlayClickSound);
+
+        //Wird beim Klicken ausgeführt
+        /*returnBtnHTP.onClick.AddListener(PlayClickSound);
+        returnBtnSettings.onClick.AddListener(PlayClickSound);
+        returnBtnEndTitle.onClick.AddListener(PlayClickSound);
+        returnBtnEndTitle.onClick.AddListener(PlayClickSound);*/
+
+        foreach (var button in btns)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
     }
 
     void PlayClickSound()
     {
+        //Abspielen des Sounds
         audioSource.PlayOneShot(clickSound, 1f);
     }
 }
