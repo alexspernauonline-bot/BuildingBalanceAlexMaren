e65e75d83a5cc2ce61de3271df2ce13ced7cda46
nightwatcher23 <maren@rautmann.eu>
2026-07-28 11:43:12 +0200

    mute button

--- Assets/Scripts/UI-Scrips/ControlVolume.cs
+++ Assets/Scripts/UI-Scrips/ControlVolume.cs
@@ -7,26 +7,46 @@ public class ControlVolume : MonoBehaviour
 {
     //Verbindung zum Volume-Slider
     public Slider volumeSlider;
+    public Toggle muteToggle;
 
-    //Variable für die Lautstärke
-    public float setVolume;
+    private float volume;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Mögliche gespeicherte Preferenzen
-        float volume = PlayerPrefs.GetFloat("Volume", 1f);
+        volume = PlayerPrefs.GetFloat("Volume", 1f);
 
         //Slider wird an ursprüngliche Lautstärke angepasst
         volumeSlider.value = volume;
 
         volumeSlider.onValueChanged.AddListener(SetVolume);
+        muteToggle.onValueChanged.AddListener(delegate
+        {
+            ToggleValueChanged(muteToggle);
+        });
+    }
+
+    private void ToggleValueChanged(Toggle change)
+    {
+        if (muteToggle.isOn)
+        {
+            volume = 0f;
+        }
+        else
+        {
+            volume = volumeSlider.value;
+        }
+
+        SetVolume(volume);
     }
 
     private void SetVolume(float volume)
     {
         //Stellt Lautstärke basierend auf Slider ein
         AudioListener.volume = volume;
+
+        print(AudioListener.volume);
 
         //Speichert eingestellte Lautstärke für die anderen Szenen
         PlayerPrefs.SetFloat("Volume", volume);
